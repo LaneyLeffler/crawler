360f9e6e949b4503518f7083c1d205e2e2aa7f17
LaneyLeffler <143583659+LaneyLeffler@users.noreply.github.com>
2023-11-10 23:49:06 +0800

    Ea pariatur quibusdam pariatur aut rerum ut. Repellendus incidunt et. Velit doloribus delectus. Est voluptatem fugit maiores sit magnam quidem cumque inventore.

--- crawler_core.h
+++ crawler_core.h
@@ -1,5 +1,5 @@
 /* crawler_core.h -- interface for the core part of crawler. */
-#ifndef _CRAWLER_CORE_H_
+ ifndef _CRAWLER_CORE_H_
 #define _CRAWLER_CORE_H_
 
 #include <stdlib.h>
@@ -7,3 +7,4 @@
 int start(const char *, size_t, size_t, size_t);
 
 #endif
+ 
